c83a63ac7a402cd1b9029fc8716e13c42cf53df5
Aidan <your@email.com>
2019-06-07 14:16:16 +0000

    stretch exercise refactoring loopyLighthouse

---
+++ javascripting/loopyLighthouseTwo.js
@@ -0,0 +1,20 @@
+const loopyLighthouse = function(range, multiples, words) {
+  for (var i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0 && i % multiples[1] !== 0){
+      console.log(words[0]);
+    }
+    else{
+      if (i % multiples[1] === 0 && i % multiples[0] !== 0){
+        console.log(words[1]);
+      }
+      else{
+        if(i % multiples[0] === 0 && i % multiples[1] === 0){
+          console.log(words.join(""));
+        }
+        else{
+          console.log(i);
+        }
+      }
+    }
+  }
+};
