d94db7358f8a8ad251d5c21225056647080ec319
Aidan <your@email.com>
2019-06-24 23:22:39 +0000

    tail end

---
+++ w1/lotide/tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual == expected) {
+    console.log(`✅✅✅ Assertion Passed \"${actual}\" === \"${expected}\"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed \"${actual}\" !== \"${expected}\"`);
+  }
+};
+
+const tail = function(array){
+  let newArray = array.slice(1);
+  return newArray;
+}
+
+const result = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+  assertEqual(array1[i], array2[i]);
+  }
+};
+
+result(tail([5,6,7]), [6,7]);
