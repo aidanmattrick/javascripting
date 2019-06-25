437ed84699c1fb280781a38833f83bc3c62e137e
Aidan <your@email.com>
2019-06-25 23:36:40 +0000

    edited to make sure returning a different array than source

--- w1/lotide/without.js
+++ w1/lotide/without.js
@@ -16,14 +16,20 @@ const eqArrays = function (array1, array2) {
 
 
 const without = function (source, itemsToRemove) {
-  for (let i = 0; i < source.length; i++) {
+  let result = source;
+  for (let i = 0; i < result.length; i++) {
     for (let j = 0; j < itemsToRemove.length; j++) {
-      if (source[i] === itemsToRemove[j]) source.splice(i, 1);
+      if (result[i] === itemsToRemove[j]) result.splice(i, 1);
     }
   }
-  return source;
+  return result;
 };
 
 without([1, 2, 3], [1]) // => [2, 3]
 without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
 assertArraysEqual(without([1, 2, 3], [1]), [2,3]);
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
