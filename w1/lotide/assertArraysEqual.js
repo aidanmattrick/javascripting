7f561702308c141463824edfdf796bc6c52d314f
Aidan <your@email.com>
2019-06-25 22:56:14 +0000

    fixed to accurately handle criteria

--- w1/lotide/assertArraysEqual.js
+++ w1/lotide/assertArraysEqual.js
@@ -1,14 +1,19 @@
 
 const assertArraysEqual = function (array1, array2) {
-  if (array1.length != array2.length) {
-    console.log(`🛑🛑🛑 Assertion Failed \"${array1}\" !== \"${array2}\"`);
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      console.log("🛑🛑🛑 Assertion Failed" + `\"${array1}\" !== \"${array2}\"`);
-    }
+  if (eqArrays(array1, array2)) {
     console.log("✅✅✅ Assertion Passed" +  `\"${array1}\" === \"${array2}\"`);
   }
+  else {
+    console.log("🛑🛑🛑 Assertion Failed" + `\"${array1}\" !== \"${array2}\"`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length != array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
 };
 
 assertArraysEqual(["banker", 2, 3], ["banker", 2, 3]);
