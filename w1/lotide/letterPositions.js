4e5601b08469530e3b5393a3ec2a10132afe571b
Aidan <your@email.com>
2019-06-26 20:22:46 +0000

    added test case

--- w1/lotide/letterPositions.js
+++ w1/lotide/letterPositions.js
@@ -1,10 +1,10 @@
 //TEST
 const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log("✅✅✅ Assertion Passed" + `\"${array1}\" === \"${array2}\"`);
+    console.log("✅✅✅ Assertion Passed: " + `\"${array1}\" === \"${array2}\"`);
   }
   else {
-    console.log("🛑🛑🛑 Assertion Failed" + `\"${array1}\" !== \"${array2}\"`);
+    console.log("🛑🛑🛑 Assertion Failed: " + `\"${array1}\" !== \"${array2}\"`);
   }
 };
 
@@ -29,5 +29,5 @@ const letterPositions = function (sentence) {
   return results;
 };
 
-console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello").e, [1]);
 
