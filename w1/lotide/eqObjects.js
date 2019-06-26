c653cf247d29d963589e86bda2c09c9e37e6b678
Aidan <your@email.com>
2019-06-26 23:34:10 +0000

    fixed to handle arrays

--- w1/lotide/eqObjects.js
+++ w1/lotide/eqObjects.js
@@ -7,23 +7,37 @@ const assertEqual = function (actual, expected) {
   }
 };
 
+//COMPARE ARRAYS
+const eqArrays = function (array1, array2) {
+  if (array1.length != array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+
 //FUNC
-const eqObjects = function (object1, object2) {
-  let check = false;
-  for (let [key, value] of Object.entries(object1)) {
-    for (let [key2, value2] of Object.entries(object2)) {
-      if (key == key2) {
-        if (value == value2) check = true;
-        else return false;
-      }
+const eqObjects = function (obj1, obj2) {
+  //let check = false;
+  if (eqArrays(Object.keys(obj1).sort(), Object.keys(obj2).sort()) == false) return false;
+
+  for (const key in obj1) {
+    if (Array.isArray(obj1[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) return false;
+    }
+    else {
+      if (obj1[key] !== obj2[key]) return false;
     }
   }
-  if (Object.keys(object1).length != Object.keys(object2).length) return false;
-  if (check == true) return true;
+  return true;
 };
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
 
 const abc = { a: "1", b: "2", c: "3" };
 
