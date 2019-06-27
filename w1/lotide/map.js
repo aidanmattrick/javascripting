73b20ebf372faa3900e948d4ee678b1a03a489ca
Aidan <your@email.com>
2019-06-27 20:16:34 +0000

    fixed for callback and added test case

--- w1/lotide/map.js
+++ w1/lotide/map.js
@@ -9,10 +9,10 @@ const map = function(array, callback){
 //TEST
 const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log("✅✅✅ Assertion Passed" +  `\"${array1}\" === \"${array2}\"`);
+    console.log("✅✅✅ Assertion Passed: " +  `\"${array1}\" === \"${array2}\"`);
   }
   else {
-    console.log("🛑🛑🛑 Assertion Failed" + `\"${array1}\" !== \"${array2}\"`);
+    console.log("🛑🛑🛑 Assertion Failed: " + `\"${array1}\" !== \"${array2}\"`);
   }
 };
 
@@ -33,6 +33,10 @@ const map = function(array, callback) {
   return results;
 }
 
-console.log(map(words));
+const randomFunc = function(x){
+  return x;
+};
 
-//assertArraysEqual(map(words, callback), words);
+//console.log(map(words, randomFunc));
+
+assertArraysEqual(map(words, randomFunc), words);
