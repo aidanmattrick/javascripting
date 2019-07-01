6c26c2524153a8027fccbdd3c28031dff444782f
Aidan <your@email.com>
2019-07-01 17:18:36 +0000

    exported these functions for testing

--- w1/lotide/head.js
+++ w1/lotide/head.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed \"${actual}\" === \"${expected}\"`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed \"${actual}\" !== \"${expected}\"`);
-  }
-};
-// TEST CODE
-const head = function(array){
-  return array[0];
-}
+const assertEqual = require('./assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const head = function(array) {
+  return array[0];
+};
+
+module.exports = head;
