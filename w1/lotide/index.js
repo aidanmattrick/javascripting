7250469e0194fcdec3c78058df540f6fa0c43b00
Aidan <your@email.com>
2019-07-01 22:26:11 +0000

    added index file with all of functions in Lotide

---
+++ w1/lotide/index.js
@@ -0,0 +1,39 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+const assertEqual = require("./assertEqual");
+const assertArraysEqual = require("./assertArraysEqual");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const assertObjectsEqual = require("./assertObjectsEqual");
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  eqObjects,
+  assertObjectsEqual,
+};
+
